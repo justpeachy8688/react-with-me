3f26967f932df40a3d6660080bc328cd3bd10e19
Taylor Sanders <tsanders3515@gmail.com>
2021-05-13 15:54:31 -0800

    menu items, categories and portfolio pics for Portfolio page

---
+++ src/components/Portfolio/portfolios.js
@@ -0,0 +1,44 @@
+import RandomPasswordGenerator from "../../img/PortfolioImages/RandomPassword.png";
+import WatchNextPic from "../../img/PortfolioImages/WatchNextPic.png";
+import EmployeeDirectory from "../../img/PortfolioImages/EmpDirectory.png";
+import WeatherDashboard from "../../img/PortfolioImages/Weather.png";
+import Envision from "../../img/PortfolioImages/Envision.png";
+import LetsVenture from "../../img/PortfolioImages/LetsVenture.png"
+
+const portfolios = [
+    {
+        id: 1,
+        category: "",
+        icon1: "",
+        icon2: "",
+        image: WatchNextPic,
+        title: "WatchNext"
+    },
+    {
+        id: 1,
+        category: "",
+        icon1: "",
+        icon2: "",
+        image: LetsVenture,
+        title: "WatchNext"
+    },
+    {
+        id: 1,
+        category: "",
+        icon1: "",
+        icon2: "",
+        image: WeatherDashboard,
+        title: "WatchNext"
+    },
+    {
+        id: 1,
+        category: "",
+        icon1: "",
+        icon2: "",
+        image: Envision,
+        title: "WatchNext"
+    }
+]
+
+
+export default portfolios;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Categories from '../components/Categories';
+import Title from '../components/Title/Title';
+import MenuItems from "../components/MenuItems";
+import portfolios from "../components/Portfolio/portfolios";
+import { useState } from "react"
 
 function Portfolio() {
+    const [categories, setCategories] = useState(null);
+    const [menuItems, setMenuItems] = useState(portfolios);
     return (
-        <div>
-            <h1>Portfolio Page</h1>
+        <div className="PortfolioPage">
+            <div className="title">
+                <Title title={'Portfolio'} span={'portfolios'} />
+            </div>
+            <div className="portfolios-data">
+                <Categories />
+                <MenuItems menuItem={menuItems} />
+            </div>
         </div>
     )
 }
