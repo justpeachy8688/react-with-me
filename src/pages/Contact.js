03107d8c18af5847d149894c6b9da200c2a00873
Taylor Sanders <tsanders3515@gmail.com>
2021-05-18 23:01:07 -0800

    took away title underline

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,7 +17,7 @@ function ContactPage() {
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1931.1816426959767!2d-149.78371251917838!3d61.051225882380834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x56c89b72f64478d7%3A0xe14ce56cdba871f7!2sPotter%20View%20Cir%2C%20Anchorage%2C%20AK%2099516!5e0!3m2!1sen!2sus!4v1620851185889!5m2!1sen!2sus" width="600" height="450" style={{ border: 0 }} allowFullScreen="" loading="lazy"></iframe>
                 </div>
                 <div className="contact-sect">
-                    <ContactItem icon={phone} text1={'907-201-2131'} text2={'124-453-4443'} title={'Phone'} />
+                    <ContactItem icon={phone} text1={'907-201-2131'} title={'Phone'} />
                     <ContactItem icon={email} text1={'tsanders3515@gmail.com'} text2={''} title={'Email'} />
                     <ContactItem icon={location} text1={'Anchorage, AK'} text2={''} title={'Location'} />
                 </div>
